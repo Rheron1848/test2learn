44edc287a0b491472b47f15689d6ee0fe8364a7e
Pulu1848 <rheronren@outlook.com>
2024-07-18 01:32:53 +0800

    按照python神经网络编程一书中的代码进行尝试，初始化文件

---
+++ neuralnet.py
@@ -0,0 +1,26 @@
+import numpy
+
+
+class NeuralNetwork:
+    def __init__():
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.lr = learningrate
+
+        pass
+    def trian():
+        pass
+
+    def query():
+        pass
+
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+
+learning_rate = 0.3
+
+n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
