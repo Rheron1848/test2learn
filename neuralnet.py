0f227e6b6d0bb5f6a81126598e34fc2829c31ae8
Pulu1848 <rheronren@outlook.com>
2024-07-18 01:59:23 +0800

    1

--- neuralnet.py
+++ neuralnet.py
@@ -11,6 +11,20 @@ class NeuralNetwork:
 
         pass
     def trian():
+        self.wih = (numpy.random.rand(self.hnodes, self.inodes) - 0.5)
+        self.who = (numpy,random.rand(self.onodes, self.hnodes) - 0.5)
+        # wih = weights input to hidden layer
+        # who = weights hidden to output layer
+        # 从随机数构建两个链接权重矩阵
+
+        #也可以使用更高级的随机数构建方法,使用正态概率分布采样权重
+        #self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        #self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        '''
+        函数 numpy.random.normal(loc=0.0, scale=1.0, size=None) 的参数含义如下：
+        loc：正态分布的均值（平均值）。默认为 0.0。
+        scale：正态分布的标准差。默认为 1.0。
+        size：输出的形状。如果给定一个整数，则返回一个具有该长度的一维数组；如果给定一个元组，则返回一个具有指定维度的数组。'''
         pass
 
     def query():
